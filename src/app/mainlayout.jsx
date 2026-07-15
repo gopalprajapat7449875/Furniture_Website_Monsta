b70404646993da4854fedaf88e362c7121fb703f
gopalprajapat7449875 <gopalprajapat7449875@gmail.com>
2026-07-15 18:58:59 +0530

    dfzb

--- src/app/mainlayout.jsx
+++ src/app/mainlayout.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { store } from './reduxwork/Store'
-import AuthLayout from '../proxy'
+
 
 
 export default function Mainlayout({ children }) {
