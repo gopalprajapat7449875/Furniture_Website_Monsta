3d5451b9c49bfc664f8172133b225cc9c4ca1844
gopalprajapat7449875 <gopalprajapat7449875@gmail.com>
2026-07-18 12:34:06 +0530

    ev

--- src/app/components/about/Why-Choose.jsx
+++ src/app/components/about/Why-Choose.jsx
@@ -6,7 +6,7 @@ export default function WhyChoose() {
 
 
     const [data, setdata] = useState(null)
-    
+
 
 
     let apibaseurl = process.env.NEXT_PUBLIC_APIBASEURL
@@ -33,23 +33,23 @@ export default function WhyChoose() {
                     <div className='grid pt-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 '>
 
 
-{data?.map((item,i)=>(
- <div className='text-center py-4 px-6'>
-                            <img className='mx-auto ' src= {item._image} alt='89df96b6-b70d-463b-affb' />
-                            <h6 className='text-[15px] font-serif pt-2'>
-                              {item._WhyChooseTital}
+                        {data?.map((item, i) => (
+                            <div className='text-center py-4 px-6'>
+                                <img className='mx-auto ' src={item._image} alt='89df96b6-b70d-463b-affb' />
+                                <h6 className='text-[15px] font-serif pt-2'>
+                                    {item._WhyChooseTital}
 
-                            </h6>
-                            <p className='text-[12px] text-neutral-600 pt-2 '>
-  {item._WhyChooseDiscription}
-                            </p>
-                        </div>
-                      
-))}
-                       
+                                </h6>
+                                <p className='text-[12px] text-neutral-600 pt-2 '>
+                                    {item._WhyChooseDiscription}
+                                </p>
+                            </div>
+
+                        ))}
+
 
                     </div>
-                  
+
                 </div>
             </div>
         </>
